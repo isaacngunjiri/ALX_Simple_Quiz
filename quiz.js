da026b166cd368c21e8c7f21306b11bbce16c658
isaacngunjiri <49683839+isaacngunjiri@users.noreply.github.com>
2024-10-06 20:02:03 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,9 +2,10 @@ function checkAnswer() {
     const correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked');
 
+    const feedbackElement = document.getElementById('feedback');
+
     if (userAnswer) {
         const userAnswerValue = userAnswer.value;
-        const feedbackElement = document.getElementById('feedback');
 
         if (userAnswerValue === correctAnswer) {
             feedbackElement.textContent = "Correct! Well done.";
@@ -12,7 +13,6 @@ function checkAnswer() {
             feedbackElement.textContent = "That's incorrect. Try again!";
         }
     } else {
-        const feedbackElement = document.getElementById('feedback');
         feedbackElement.textContent = "Please select an answer!";
     }
 }
