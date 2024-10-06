64fb63003576d1ed465d024d83b273cfcbe90474
isaacngunjiri <49683839+isaacngunjiri@users.noreply.github.com>
2024-10-06 19:56:08 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,32 +1,19 @@
-
 function checkAnswer() {
-    
-    const correctAnswer = "4"; 
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    const feedbackElement = document.getElementById('feedback');
 
-    
-    const userAnswer = document.querySelector('input[name="quiz"]:checked'); // Select the checked radio button
-
-    
     if (userAnswer) {
-        // Access the value property of the selected radio button
         const userAnswerValue = userAnswer.value;
 
-        // Compare the User’s Answer with the Correct Answer
-        const feedbackElement = document.getElementById('feedback'); // Get the feedback element
-
         if (userAnswerValue === correctAnswer) {
-            // User's answer is correct
-            feedbackElement.textContent = "Correct! Well done."; 
+            feedbackElement.textContent = "Correct! Well done.";
         } else {
-            // User's answer is incorrect
-            feedbackElement.textContent = "That's incorrect. Try again!"; 
+            feedbackElement.textContent = "That's incorrect. Try again!";
         }
     } else {
-        // No answer selected
-        const feedbackElement = document.getElementById('feedback');
-        feedbackElement.textContent = "Please select an answer!"; 
+        feedbackElement.textContent = "Please select an answer!";
     }
 }
-
 
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
